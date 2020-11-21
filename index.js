395ae235b859403b1ab620b8c7dce2a160455706
Ilya <kompfloss@gmail.com>
2020-11-21 22:50:10 +0300

    Test pug integration

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.get('/test', (req, res) => {
+  res.render('index');
 });
 
 const port = process.env.PORT || 5000;
